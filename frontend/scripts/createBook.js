57d20770f95a0d63ba0a902d62c032e5d4ac3944
Raytopjampa <rayanne.rodrigues@academico.ifpb.edu.br>
2020-10-30 14:25:00 -0300

    fase de teste do upload

--- frontend/scripts/createBook.js
+++ frontend/scripts/createBook.js
@@ -1,25 +1,18 @@
 const host = "http://127.0.0.1:3333";
 const form = document.getElementById('create-form');
 
-form.addEventListener('submit', async (event)=>{
+form.addEventListener("submit", (event)=>{
     event.preventDefault();
-
+   
     
     const fd= new FormData(form);
 
-    const response = await fetch(`${host}/book`,{
+    fetch(`${host}/book`,{
         method: "POST",
         body:fd,
     });
-
+}
+);
 //
-    // Handless server response
-    if (response.status === 200 || response.status === 201){
-        alert("Livro Cadastrado com sucesso");
-    }else{
-        alert("Ops... Ocorreu um erro durante o cadastro");
-    }
-});
-
 
 
